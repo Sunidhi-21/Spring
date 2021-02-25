975431c111790944f36dd9c41bfb2c166cc8b805
Sunidhi-21 <sunidhi.n.xworkz@gmail.com>
2021-02-25 10:41:32 +0530

    shop

--- java/com/xworkz/shop/service/ShopServiceImpl.java
+++ java/com/xworkz/shop/service/ShopServiceImpl.java
@@ -21,13 +21,49 @@ public class ShopServiceImpl implements ShopService {
 		if (shop != null) {
 			String name = shop.getShopName();
 
-			if (name != null && !name.isEmpty() && name.length() < 15) {
+			if (name.length()>=3 && name.length()<=12) {
 				System.out.println("name is valid");
 				valid = true;
 			} else {
 				System.out.println("name is invalid");
 				valid = false;
 			}
+			
+			String location=shop.getLocation();
+			if(location.length()>=3 && location.length()<=30) {
+				System.out.println("location is valid");
+				valid = true;
+			}else {
+				System.out.println("location is invalid");
+				valid=false;
+			}
+			
+			String oname = shop.getOwnerName();
+				if(valid) {
+					if(oname!=null && oname.length()<=12 && oname.length()>=4) {
+						System.out.println("ownername is valid");
+						valid=true;
+					} else {
+						System.out.println("ownername is invalid");
+						valid=false;
+					}
+				
+				int open=shop.getOpen();	
+				if(open==1 || open==0) {
+					if(open==1) {
+						System.out.println("shop is open");
+					    valid=true;
+					}
+					if(open==0)
+					{
+						System.out.println("shop is closed");
+						valid=true;
+					}
+				}else {
+					System.out.println("open invalid");
+					valid=false;
+				}
+				
 
 			if (valid) {
 				System.out.println("shop is valid, can save");
@@ -38,7 +74,8 @@ public class ShopServiceImpl implements ShopService {
 
 		}
 
-		return false;
+		
 	}
-
+		return valid;
+	}
 }
